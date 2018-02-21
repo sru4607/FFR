a0ad33c1fae0cf0dfa17f26b17dcc823f2d9c274
rkb2204 <rkb2204@IGMOH1380-32>
2018-02-21 13:02:02 -0500

    Ryan did the thing right this time

--- TestGame/TestGame/Game1.cs
+++ TestGame/TestGame/Game1.cs
@@ -5,7 +5,7 @@ using Microsoft.Xna.Framework.Input;
 namespace TestGame
 {
     /// <summary>
-    /// This is the main type for your game.
+    /// Dark Souls is best game
     /// </summary>
     public class Game1 : Game
     {
