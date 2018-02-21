6a8c3838366839c1b23a3b4b782e4fc731877042
kmk8334 <kmk8334@IGMOH1380-31>
2018-02-21 13:03:13 -0500

    Changed Game1.cs line 8

--- TestGame/TestGame/Game1.cs
+++ TestGame/TestGame/Game1.cs
@@ -5,7 +5,7 @@ using Microsoft.Xna.Framework.Input;
 namespace TestGame
 {
     /// <summary>
-    /// This is the main type for your game.
+    /// This is the main type for your game. These are extra words.
     /// </summary>
     public class Game1 : Game
     {
