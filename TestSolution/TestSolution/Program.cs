9edde118c6a233a2f88743344913c6421e2fd6d3
sru4607 <sru4607@IGMOH1380-29>
2018-02-21 12:50:27 -0500

    Original Commit by: Samuel Unferth

---
+++ TestSolution/TestSolution/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Edit Line 14
+            Console.WriteLine("Hello World");
+        }
+    }
+}
