ba8eed032181e2687c842be229b55c7b67c5ac37
sunferth <srunferth@carolina.rr.com>
2018-02-28 18:58:29 -0500

    Created a variety of classes

---
+++ ActualGame/ActualGame/PhysicsObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActualGame
+{
+    class PhysicsObject
+    {
+        int x;
+        int y;
+        int velX;
+        int velY;
+
+        void Move(int x, int y)
+        {
+            x = x + velX;
+            y = y + velY;
+        }
+        void Gravity()
+        {
+            velY + 9.8*
+        }
+
+
+    }
+}
