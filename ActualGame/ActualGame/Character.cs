88371669b66704145f9d9e48ca7f325d6e09c505
sunferth <srunferth@carolina.rr.com>
2018-03-06 16:43:10 -0500

    Renamed Physics -> gameobject and worked on character

--- ActualGame/ActualGame/Character.cs
+++ ActualGame/ActualGame/Character.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace ActualGame
 {
-    class Character : PhysicsObject
+    class Character : GameObject
     {
         public Character()
+            :base()
         {
-
+            
+        }
+        public override void Update()
+        {
+            base.Update();
+        }
+        public override void Draw(SpriteBatch sb)
+        {
+            base.Draw(sb);
         }
     }
 }
---
+++ ActualGame/ActualGame/GameObject.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ActualGame
+{
+    class GameObject
+    {
+        Rectangle rect;
+        double velX;
+        double velY;
+        Texture2D texture;
+
+
+        private void Move(double x, double y)
+        {
+            X = (int)(X + velX);
+            Y = (int)(Y + velY);
+        }
+        private void Gravity(double time)
+        {
+            velY += 9.8 * time;
+        }
+        virtual public void Update()
+        {
+
+        }
+        virtual public void Draw(SpriteBatch sb)
+        {
+            sb.Draw(texture, rect, Color.White);
+        }
+        public int X
+        {
+            get
+            {
+                return rect.X;
+            }
+            set
+            {
+                rect = new Rectangle(value, Y, Width, Height);
+            }
+        }
+        public int Y
+        {
+            get
+            {
+                return rect.X;
+            }
+            set
+            {
+                rect = new Rectangle(X, value, Width, Height);
+            }
+        }
+        public int Width
+        {
+            get
+            {
+                return rect.X;
+            }
+            set
+            {
+                rect = new Rectangle(X, Y, value, Height);
+            }
+        }
+        public int Height
+        {
+            get
+            {
+                return rect.Height;
+            }
+            set
+            {
+                rect = new Rectangle(X, Y, Width, value);
+            }
+        }
+
+
+    }
+}
