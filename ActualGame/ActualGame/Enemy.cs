3e39b5981c39dd1ed26e3097b5d4dfa040fe8e8d
Samuel Unferth <sru4607@g.rit.edu>
2018-03-06 17:50:21 -0500

    Added AI and combat classes, added more empty methods into the Enemy, NPC, and Player class

--- ActualGame/ActualGame/Enemy.cs
+++ ActualGame/ActualGame/Enemy.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace ActualGame
 {
     class Enemy : Character
     {
+        AI mainAi;
         public Enemy()
+        {
+            mainAi = new AI();
+        }
+        public override void Update()
+        {
+
+        }
+        public override void Draw(SpriteBatch sb)
         {
 
         }
--- ActualGame/ActualGame/NPC.cs
+++ ActualGame/ActualGame/NPC.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace ActualGame
 {
     class NPC : Character
     {
         public NPC()
+        {
+
+        }
+        public override void Update()
+        {
+
+        }
+        public override void Draw(SpriteBatch sb)
         {
 
         }
