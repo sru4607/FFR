d062a80ff2cfd17bdafb62560d6ee016cd6bf50a
rkb2204 <rkb2204@IGMOH1380-32>
2018-03-21 11:58:41 -0400

    Added methods and a field to character

--- ActualGame/ActualGame/Character.cs
+++ ActualGame/ActualGame/Character.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace ActualGame
 {
-    class Character : GameObject
+    class Character : GameObject, ICombat
     {
         //Fields
         int hp;
+        Rectangle mBox;
 
         //Properties
         int HP
@@ -19,23 +21,73 @@ namespace ActualGame
             set { hp = value; }
         }
 
-
+        /// <summary>
+        /// Creates a new instance of the Character class
+        /// </summary>
         public Character()
             :base()
+        {
+            hp = 1;
+            mBox = new Rectangle();
+        }
+
+        /// <summary>
+        /// the Character makes a melee attack
+        /// </summary>
+        public void MAttack()
         {
             
         }
 
+        /// <summary>
+        /// the Character makes a ranged attack
+        /// </summary>
+        public void RAttack()
+        {
 
+        }
+
+        /// <summary>
+        /// Updates the status of a Character object
+        /// </summary>
         public override void Update()
         {
             base.Update();
         }
 
-
+        /// <summary>
+        /// Draws a Character object
+        /// </summary>
+        /// <param name="sb">the related spritebatch</param>
         public override void Draw(SpriteBatch sb)
         {
             base.Draw(sb);
+        }
+
+        /// <summary>
+        /// The Character dies
+        /// </summary>
+        public void Die()
+        {
+
+        }
+
+        /// <summary>
+        /// The Character's hp reduces
+        /// </summary>
+        /// <param name="damageAmount">the amount of damage to take</param>
+        public void TakeDamage(int damageAmount)
+        {
+
+        }
+
+        /// <summary>
+        /// The character is stunned for a bit
+        /// </summary>
+        /// <param name="stunTime">the time the character is stunned</param>
+        public void Stun(double stunTime)
+        {
+
         }
     }
 }
