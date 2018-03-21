2e300978097b956ada2d18400ccd1a87f8c141e0
jak4694 <jak4694@IGMOH1380-30>
2018-03-21 12:10:52 -0400

    Updated Player class to incorporate ICombat (some methods need to be implemented later)

--- ActualGame/ActualGame/Player.cs
+++ ActualGame/ActualGame/Player.cs
@@ -7,20 +7,49 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace ActualGame
 {
-    class Player : Character
+    class Player : Character, ICombat
     {
         public Player()
             :base()
         {
 
         }
+
         public override void Update()
         {
             base.Update();
         }
+
         public override void Draw(SpriteBatch sb)
         {
             base.Draw(sb);
+        }
+
+        /// <summary>
+        /// Reduces the health of the Player when they take damage, and calls the Die method if necessary
+        /// </summary>
+        /// <param name="damageAmount">The amount of damage taken</param>
+        public void TakeDamage(int damageAmount)
+        {
+            if (damageAmount > hp)
+            {
+                hp = 0;
+                Die();
+            }
+            else
+                hp -= damageAmount;
+        }
+
+        //TODO: Method to call that should update the game to signal the Player has died
+        public void Die()
+        {
+
+        }
+
+        //TODO: Method to stun the player for the amount of time chosen
+        public void Stun(double timeToStun)
+        {
+
         }
     }
 }
