e21d847d0b771e6f50c4c6e641cc5cbffe53c396
Jason Kulp <jasonkulp78@gmail.com>
2018-03-26 23:03:43 -0400

    Break statement in GameObject Collision method

--- ActualGame/ActualGame/GameObject.cs
+++ ActualGame/ActualGame/GameObject.cs
@@ -112,6 +112,7 @@ namespace ActualGame
                             if(this.hitbox.CheckCollision(temp[i].HitBox))
                             {
                                 StepBack();
+								break;
                             }
                         }
                     }
