9fc4a4ed51aead986c47d9e8b850f13d6f3c490c
jonmura1999 <jonmura1999@gmail.com>
2018-03-28 18:47:22 -0400

    Enabled auto scroll so that maps will automatically enable scrollbars if bigger than default, added new map window so maps could be created at any size, added functionality to save button in drop menu

--- MuraMapEditorV2/MuraMapEditorV2/Program.cs
+++ MuraMapEditorV2/MuraMapEditorV2/Program.cs
@@ -16,7 +16,7 @@ namespace MuraMapEditorV2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MapEditor());
         }
     }
 }
