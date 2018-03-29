14c6dff6b586bfefbbfa1b6a2334250cfc7cf17d
Kevin K <kmk8334@kgcoe-git.rit.edu>
2018-03-28 21:38:48 -0400

    Implemented generic AI movement; Enemy.cs can now be drawn to the screen; updated Game1.LoadContent() with comments and formatting

--- ActualGame/ActualGame/Enemy.cs
+++ ActualGame/ActualGame/Enemy.cs
@@ -12,7 +12,7 @@ namespace ActualGame
     class Enemy : Character, ICombat
     {
         #region Fields
-            AI mainAi;
+        AI mainAi;
         #endregion
 
         #region Properties
@@ -20,55 +20,69 @@ namespace ActualGame
         #endregion
 
         #region Constructor
-            /// <summary>
-            /// Creates a generic Enemy
-            /// </summary>
-            public Enemy()
-                : base()
-            {
-                mainAi = new AI(PatrolType.Standing);
-            }
+        /// <summary>
+        /// Creates a generic Enemy
+        /// </summary>
+        public Enemy()
+            : base()
+        {
+            // Initialize the AI pattern
+            mainAi = new AI(PatrolType.Moving);
+
+            // Temporary values to render to screen
+            rect.X = 400;
+            rect.Y = 300;
+            rect.Width = 128;
+            rect.Height = 128;
+        }
         #endregion
 
         #region Methods
-            public new void TakeDamage(int damageAmount)
+        public new void TakeDamage(int damageAmount)
+        {
+            if (damageAmount >= hp)
             {
-                if (damageAmount > hp)
-                {
-                    hp = 0;
-                    Die();
-                }
-                else
-                    hp -= damageAmount;
+                hp = 0;
+                Die();
             }
+            else
+                hp -= damageAmount;
+        }
 
 
-            public new void Die()
-            {
+        public new void Die()
+        {
+            // TODO: Implement during combat
+        }
 
-            }
 
-
-            public new void Stun(int stunFrames)
-            {
-
-            }
+        public new void Stun(int stunFrames)
+        {
+            // TODO: Implement during combat
+        }
         #endregion
 
         #region Update
-            public override void Update()
-            {
-                // TODO Implement AI movement
+        public override void Update()
+        {
+            // TODO: Update so rect.Y is moved in the same call
+            // NOTE: Do NOT call .MoveAI() twice, it will count as two frames of movement
+            // NOTE: Also only moves in the X direction right now
+            rect.X += (int)mainAi.MoveAI();
 
-                base.Update();
-            }
+            base.Update();
+        }
         #endregion
 
         #region Draw
-            public override void Draw(SpriteBatch sb)
-            {
-                base.Draw(sb);
-            }
+        public override void Draw(SpriteBatch sb)
+        {
+            if(mainAi.FacingRight)
+                sb.Draw(texture, rect, null, Color.White, 0, new Vector2(0, 0), SpriteEffects.None, 0);
+            // Draws to the screen with a horizontal flip if the AI is facing left
+            else
+                sb.Draw(texture, rect, null, Color.White, 0, new Vector2(0,0), SpriteEffects.FlipHorizontally, 0);
+        }
         #endregion
     }
 }
