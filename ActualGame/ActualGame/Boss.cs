ca45615b6000b815b5fe4d8ef98929c8d7323aee
sru4607 <sru4607@g.rit.edu>
2018-04-27 11:22:39 -0400

    Revert "Revert "Boss work""
    This reverts commit ee932615f0d673fdd6ed62acf43cb0620ea3ede6.

--- ActualGame/ActualGame/Boss.cs
+++ ActualGame/ActualGame/Boss.cs
@@ -12,13 +12,19 @@ namespace ActualGame
     class Boss : Character
     {
         int maxHP;
+        int theta;
+        Rectangle maceSource;
+        Rectangle bossSource;
+        Rectangle chainSource;
 
-        public Boss(Texture2D text)
-        :base(0,0, new QuadTreeNode(0,0,100,100))
+        public Boss(Texture2D text, int x, int y, int width, int height)
+        :base(x,y, new QuadTreeNode(x,y,width,height))
         {
             texture = text;
-            HP = 80;
+            HP = 100;
             maxHP = 100;
+            bossSource = new Rectangle(x, y, width, height);
+            
         }
 
 
@@ -31,6 +37,35 @@ namespace ActualGame
         {
             sb.Draw(Texture, new Rectangle(sb.GraphicsDevice.Viewport.Width/2 - 204, sb.GraphicsDevice.Viewport.Height - 54,408,40), Color.Black);
             sb.Draw(Texture, new Rectangle(sb.GraphicsDevice.Viewport.Width / 2 - 200, sb.GraphicsDevice.Viewport.Height - 50, (int)(((double)HP/maxHP)*400), 32), Color.Red);
+        }
+
+        public void UpdateBoss(GameTime gm)
+        {
+            theta += 6;
+            //Check if Boss Is Hit
+
+            //Update Chain
+            chainSource = new Rectangle((int)Math.Cos(theta / 360 * 2 * Math.PI), (int)Math.Sin(theta / 360 * 2 * Math.PI), chainSource.Width, chainSource.Height);
+            //Update Mace
+
+            //Check if Player is Hit
+            if(maceSource.Intersects(new Rectangle((int)Game1.player.position.X, (int)Game1.player.position.Y, (int)Game1.player.Size.X, (int)Game1.player.Size.Y)))
+            {
+                Game1.player.TakeDamage(1);
+            }
+
+
+            
+        }
+
+        public void DrawBoss(SpriteBatch sb)
+        {
+            //Draw Chain
+
+            //Draw Boss
+
+
+            //Draw Mace
         }
 
     }
